3e3994a33558f0ad2a1da587b7a40cb9d0157f1c
Leandro Stanzione <stanzione.leandro@gmail.com>
2018-10-30 13:08:29 -0300

    Bind views with ButterKnife

--- app/src/main/java/br/com/stanzione/uoltest/noticias/NoticiasFragment.java
+++ app/src/main/java/br/com/stanzione/uoltest/noticias/NoticiasFragment.java
@@ -3,16 +3,26 @@ package br.com.stanzione.uoltest.noticias;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 
 import java.util.List;
 
 import br.com.stanzione.uoltest.R;
 import br.com.stanzione.uoltest.data.News;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class NoticiasFragment extends Fragment implements NoticiasFragmentContract.View{
+
+    @BindView(R.id.newsRecyclerView)
+    RecyclerView newsRecyclerView;
+
+    @BindView(R.id.progressBar)
+    ProgressBar progressBar;
 
 
     public NoticiasFragment() {}
@@ -21,7 +31,12 @@ public class NoticiasFragment extends Fragment implements NoticiasFragmentContra
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_noticias, container, false);
+        View view = inflater.inflate(R.layout.fragment_noticias, container, false);
+        return view;
+    }
+
+    private void setupUi(View view){
+        ButterKnife.bind(this, view);
     }
 
     @Override
