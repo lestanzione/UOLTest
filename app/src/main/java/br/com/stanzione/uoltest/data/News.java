2909696427b5c68801026e24bbd4ad3043adcf94
Leandro Stanzione <stanzione.leandro@gmail.com>
2018-10-31 10:49:04 -0300

    Add News primary key

--- app/src/main/java/br/com/stanzione/uoltest/data/News.java
+++ app/src/main/java/br/com/stanzione/uoltest/data/News.java
@@ -3,11 +3,15 @@ package br.com.stanzione.uoltest.data;
 import com.google.gson.annotations.SerializedName;
 
 import java.util.Date;
+import java.util.UUID;
 
 import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
 
 public class News extends RealmObject {
 
+    @PrimaryKey
+    private String id = UUID.randomUUID().toString();
     @SerializedName("type")
     private String type;
     @SerializedName("title")
@@ -21,6 +25,14 @@ public class News extends RealmObject {
     @SerializedName("webview-url")
     private String webviewUrl;
 
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 
     public String getType() {
         return type;
